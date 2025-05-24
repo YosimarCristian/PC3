46f3b83e1af0a94ad20755bce0a42bdcb9bd81ae
CRISTIANSALAZAR <cristian_xl30@hotmail.com>
2025-05-24 00:52:53 -0500

    chore: guardar cambios antes de cambiar de rama

--- obj/Debug/net9.0/NewsPortal.AssemblyInfo.cs
+++ obj/Debug/net9.0/NewsPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91164478fb3cf01c0765de600a3aaa1886a8b75c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e12d9f171c348f01591211e1d396880806e2f703")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
